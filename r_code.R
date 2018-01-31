1927c82b51bdfcbd995f820f3e1d7e141a0d24d6
dongatuna <ngatuna05@gmail.com>
2018-01-30 19:19:54 -0800

    first commit of pid

---
+++ r_code.R
@@ -0,0 +1,8 @@
+x <- as.integer(runif(50, 2, 11)) #randomizes 50 numbers between 2 and 11
+
+sd(x)# shows the standard deviation of vector x
+var(x) # shows the variance of vector x
+median(x) # shows the median of vector x
+mad(x) # shows the median absolute deviation of vector x
+
+pnorm(-1, mean=0, sd=1)  
